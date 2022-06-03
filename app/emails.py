09fdbf9a6ab2b964e1faf2b5e876208998f51f1a
hewittaj <hewittaj@outlook.com>
2022-06-03 13:24:22 -0500

    working on loading all spam emails

--- app/emails.py
+++ app/emails.py
@@ -3,21 +3,41 @@
 # https://github.com/kootenpv/yagmail
 
 # Imports
+from datetime import date
 import json
 import yagmail
 
 # Main App
-user = 'ahewitt.hcoop@gmail.com'
-app_password = json.loads(open('./apppassword.json').read())['app_password']
+def sendEmails():
+    # Variables
+    user = 'ahewitt.hcoop@gmail.com'
+    app_password = json.loads(open('./apppassword.json').read())['app_password']
+    to = "ahewitt@heartlandcoop.com"
+    subject = "test email"
+    content = ['mail body content']
 
-to = "ahewitt@heartlandcoop.com"
+    # Send email
+    with yagmail.SMTP(user, app_password) as yag:
+        try:
+            yag.send(to, subject, content)
+            print("Email sent successfully")
+        except(yagmail.exceptions.YagConnectionError) as e:
+            print(e)
 
-subject = "test email"
-content = ['mail body content']
+def loadSpamEmails():
+    # Variables
+    fJson = json.loads(open('mail_by_user.json', 'r', encoding='utf-8').read())
+    date = ""
+    fromAddress = ""
+    subject = ""
+    
+    for email, value in fJson.items():
+        print(email)
+        for num, item in value.items():
+            #print(num)
 
-with yagmail.SMTP(user, app_password) as yag:
-    try:
-        yag.send(to, subject, content)
-        print("Email sent successfully")
-    except(yagmail.exceptions.YagConnectionError) as e:
-        print(e)
+            f"{item['attributes']['subject']}"
+            f"{item['attributes']['date']}"
+            f"{str(item['attributes']['fromAddress'][0])}"
+
+loadSpamEmails()
